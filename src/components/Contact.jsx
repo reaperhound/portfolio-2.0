cac0d27d604ccc57a95e060d75288b04ab3fe204
reaperhound <reaperhound69004@gmail.com>
2023-04-25 23:38:29 +0530

    before tailwind, added email integration for forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,95 +1,199 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import { useForm } from "react-hook-form";
+
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
+  const [buttonText, setButtonText] = useState("Send");
+  const [modalOpen, setModalOpen] = useState(false)
 
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmitHandler = async (data) => {
+    console.log(data);
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+
+    await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json;charset=utf-8",
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
+      body: JSON.stringify(data, null, 2),
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (json.success) {
+          setButtonText("Sent");
+          setModalOpen(true)
+          console.log(json.success);
+        } else {
+          alert("Not Sent");
+        }
+      })
+      .catch((error) => {
+        console.log("Form : ", error);
+      });
+
+    reset();
   };
+
+  function Modal() {
+    return (
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label className="btn" onClick={()=> {setModalOpen(false)}}>Yay!</label>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <section className="contact" id="connect">
+      {
+        modalOpen && <Modal />
+      }
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
-              }
+              {({ isVisible }) => (
+                <img
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                  src={contactImg}
+                  alt="Contact Us"
+                />
+              )}
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Get In Touch</h2>
+                  <form onSubmit={(e) => e.preventDefault()}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="hidden"
+                          value={import.meta.env.VITE_WEB3FORM_KEY}
+                          {...register("access_key")}
+                        />
+                        <input
+                          type="text"
+                          placeholder="First Name"
+                          {...register("firstName", {
+                            required: true,
+                            maxLength: 20,
+                          })}
+                          aria-invalid={errors.firstName ? "true" : "false"}
+                        />
+                        {errors.firstName?.type === "required" && (
+                          <p role="alert">First name is required</p>
+                        )}
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          placeholder="Last Name"
+                          {...register("lastName", {
+                            required: true,
+                            maxLength: 20,
+                          })}
+                          aria-invalid={errors.lastName ? "true" : "false"}
+                        />
+                        {errors.firstName?.type === "required" && (
+                          <p role="alert">Last name is required</p>
+                        )}
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="email"
+                          placeholder="Email"
+                          {...register("email", {
+                            required: true,
+                            pattern: {
+                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                              message: "invalid email address",
+                            },
+                          })}
+                          aria-invalid={errors.email ? "true" : "false"}
+                        />
+                        {errors.email && (
+                          <p role="alert">{errors.email?.message}</p>
+                        )}
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="tel"
+                          placeholder="Phone No, with country code"
+                          {...register("phone", {
+                            required: true,
+                            maxLength: 13,
+                          })}
+                          aria-invalid={errors.phone ? "true" : "false"}
+                        />
+                        {errors.phone?.type === "required" && (
+                          <p role="alert">Please enter number</p>
+                        )}
+                        {errors.phone?.type === "maxLength" && (
+                          <p role="alert">Invalid Number</p>
+                        )}
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          rows="6"
+                          placeholder="Message"
+                          {...register("message", {
+                            required: true,
+                            maxLength: 200,
+                          })}
+                          aria-invalid={errors.phone ? "true" : "false"}
+                        ></textarea>
+                        {errors.message?.type === "required" && (
+                          <p role="alert">Please Enter the message</p>
+                        )}
+                        {errors.message?.type === "maxLength" && (
+                          <p role="alert">Message is too Lengthy</p>
+                        )}
+                        <button
+                          type="submit"
+                          onClick={handleSubmit(onSubmitHandler)}
+                        >
+                          <span>{buttonText}</span>
+                        </button>
+                      </Col>
+                    </Row>
+                  </form>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
